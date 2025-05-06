ff2055ce06ef4422d5270a6ed8a392138da602a3
Reyner Veliz <reyner@reynerveliz.com>
2025-05-05 19:13:05 -0600

    Update to altitudes

--- pages/altitudes.py
+++ pages/altitudes.py
@@ -9,8 +9,8 @@ altitude_params = {"FL450": "8,000 ft Cabin @8.83 psi.  9.07 psi OFV starts to o
                    "14,800": ":red-background[CAB ALT] - Backup Avionics oxygen Deploy Set-Point", 
                    "14,500": "Outflow valve limit -ECMU controlled", 
                    "14,200": ":red-background[CAB ALT] for Field HI operation", 
-                   "12,850": "Oxygen mask deployment", 
-                   "9,500": ":red-background[CAB ALT] - EDM @ 5 seconds",
+                   "12,850": "Oxygen mask deployment - Commanded by ECMU", 
+                   "9,500": ":red-background[CAB ALT] Normal operation - EDM @ 5 seconds",
                    "8,300": "Field HI", "0": "Select an altitude"}
 
 altitude = st.radio("Select an altitude", ["FL450", "FL300", "24,800", "15,000", "14,800", "14,500", "14,200", "12,850", "9,500", "8,300", "0"], index=10)
